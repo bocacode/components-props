761ee43476c3447ca5878d2384fe7dbffb96fa98
Todd Albert <todd@bocacode.com>
2022-05-16 16:02:35 -0400

    add student to state

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Greet from './Greet';
 
+const students = [
+  { id: 1, firstName: 'Wade', lastName: 'Booth'},
+  { id: 2, firstName: 'Alex', lastName: 'Hall'},
+  { id: 3, firstName: 'Maria', lastName: 'Bruno'},
+  { id: 4, firstName: 'Arthur', lastName: 'Silva'},
+  { id: 5, firstName: 'Chole' },
+]
+
 function Main() {
+  const [studentList, setStudentList] = useState(students);
+  const handleClick = () => {
+    // add Rodrigo to our list of students
+    const rodrigo = { id: 6, firstName: 'Rodrigo' }
+    const newStudentList = [...students, rodrigo]
+    setStudentList(newStudentList)
+    document.querySelector('button').style.display = 'none'
+  }
   return (
     <main>
-      <Greet firstName="Wade" lastName="Booth" />
-      <Greet firstName="Alex" />
-      <Greet firstName="Maria" />
-      <Greet firstName="Arthur" />
-      <Greet firstName="Chole" />
+      <button onClick={handleClick}>Add Rodrigo</button>
+      {studentList.map(student => {
+        return (
+          <Greet
+            key={student.id}
+            lastName={student.lastName}
+            firstName={student.firstName} />
+          )
+      })}
     </main>
   )
 }
